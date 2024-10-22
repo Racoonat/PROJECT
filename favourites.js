3917994db7967e824c3d78bceefb3150ce31dee7
Natalia Rodriguez <Natarodred@gmail.com>
2024-10-22 10:12:22 +0300

    WEATHER APP 1.0

---
+++ favourites.js
@@ -0,0 +1,90 @@
+const apiKey = '540ba1d11ea065ab6ecf6073e792d2c9';
+const unitSelect = document.getElementById('unitSelect'); 
+const table = document.getElementById('favouritesTable');
+const tableBody = document.getElementById('favouritesBody');
+const favourites = getFavourites();
+
+
+function getFavourites() {
+    return JSON.parse(localStorage.getItem('favourites')) || [];
+}
+
+async function getWeather(city) { 
+    const { lat, lon } = await getCoordinates(city);
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unitSelect.value}`;
+    const response = await fetch(weatherUrl);
+    const data = await response.json();
+    return {
+        temperature: data.main.temp,
+        icon: data.weather[0].icon,
+        weatherDescription: data.weather[0].description
+    };
+}
+
+//--------------------------GET LAN AND LON GIVEN A CITY NAME---------------------------------------------------------------
+
+async function getCoordinates(city) {
+    const geocodingUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(geocodingUrl);
+    if (!response.ok) throw new Error('City not found');
+    const data = await response.json();
+    return { lat: data.coord.lat, lon: data.coord.lon };
+}
+
+
+async function displayFavorites() {
+    tableBody.innerHTML = '';
+
+    if (favourites.length === 0) {
+        const row = document.createElement('tr');
+        const cell = document.createElement('td');
+        cell.colSpan = 3; 
+        cell.textContent = 'Not favourites Cities yet :(';
+        row.appendChild(cell);
+        tableBody.appendChild(row);
+        return;
+    }
+
+
+    for (const favorite of favourites) {
+        
+        const weatherData = await getWeather(favorite); 
+        const row = document.createElement('tr');
+ 
+        row.addEventListener('click', () => {
+            localStorage.setItem('city', JSON.stringify(favorite));
+            window.location.href = 'index.html'; 
+        });
+
+        const cityCell = document.createElement('td');
+        cityCell.textContent = favorite;
+        row.appendChild(cityCell);
+
+        const temperatureCell = document.createElement('td');
+        temperatureCell.textContent = `${weatherData.temperature}°`; 
+        row.appendChild(temperatureCell);
+
+        const iconCell = document.createElement('td');
+        iconCell.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherData.icon}@2x.png" alt="weather icon">`;
+        row.appendChild(iconCell);
+
+        tableBody.appendChild(row);
+        
+    }
+}
+
+unitSelect.addEventListener('change', () => {
+    unitSymbol=changeUnitSymbol(unitSelect.value);
+    displayFavorites();
+});
+
+
+function changeUnitSymbol(unit) {
+    let symbols = {
+        metric: '°C',
+        imperial: '°F',
+        standard: 'K'
+    };
+    return symbols[unit];
+} 
+document.addEventListener('DOMContentLoaded', displayFavorites);
